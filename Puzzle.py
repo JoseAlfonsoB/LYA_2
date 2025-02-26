b6075de04678fb3ff1a45ec22a00cddd8d85ed39
JoseAlfonsoB <jalfonsobautista74@gmail.com>
2025-02-26 13:48:48 -0600

    Colocación del código del archivo "Puzzle.py"

---
+++ Puzzle.py
@@ -0,0 +1,26 @@
+from arbol import Nodo
+
+def buscar_solucion_BFS(estado_inicial, solucion):
+    solucionado = False
+    nodos_visitados = []
+    nodos_frontera = []
+    nodoInicial = Nodo(estado_inicial)
+    nodos_frontera.append(nodoInicial)
+    while (not solucionado)and len(nodos_frontera) !=0:
+        nodo = nodos_frontera.pop(0)
+        # Extraer nodo y añadirlo a visitados
+        nodos_visitados.append(nodo)
+        if nodo.get_datos() == solucion:
+            # Solucion encontrada
+            solucionado = True
+            return nodo
+        else:
+            # Expandir nodos hijo
+            dato_nodo = nodo.get_datos()
+
+            # Operador Izquierdo
+            hijo = [dato_nodo[1], dato_nodo[0], dato_nodo[2], dato_nodo[3]]
+            hijo_izquierdo = Nodo(hijo)
+            if not hijo_izquierdo.en_lista(nodos_visitados) \
+                and not hijo_izquierdo.en_lista(nodos_frontera):
+                nodos_frontera.append(hijo_izquierdo)
